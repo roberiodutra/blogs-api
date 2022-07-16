b1bfa56e46913ca04ba927317695816280e1f323
roberio <roberio-almeida@hotmail.com>
2022-07-16 15:15:50 -0300

    feat: add internal error message into errorM file

--- src/helpers/errorMessages.js
+++ src/helpers/errorMessages.js
@@ -1,4 +1,5 @@
 const errorMessages = {
+  INTERNAL_ERROR: { message: 'Error when trying to perform operation' },
   FIELDS_REQ: { message: 'Some required fields are missing' },
   INVALID_FIELDS: { message: 'Invalid fields' },
 };
