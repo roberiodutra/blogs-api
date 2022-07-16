bd1016036e623ba882c5750656747e24438b668c
roberio <roberio-almeida@hotmail.com>
2022-07-16 02:40:14 -0300

    feat: config postCategories migration

--- src/database/migrations/20220716051133-PostCategories.js
+++ src/database/migrations/20220716051133-PostCategories.js
@@ -2,20 +2,35 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('PostsCategories', {
+      postId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        field: 'post_id',
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        field: 'category_id',
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      },
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
   }
 };
