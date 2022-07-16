7d3b876c8293871278e892b27477c8991ce85178
roberio <roberio-almeida@hotmail.com>
2022-07-16 07:02:40 -0300

    feat: create postCategories model

---
+++ src/database/models/PostCategories.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategories = sequelize.define('PostCategories', {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  }, {
+    tableName: 'PostCategories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategories.associate = (models) => {
+    models.BlogPosts.belongsToMany(models.Categories, {
+      through: PostCategories,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      as: 'categories',
+    });
+
+    models.Categories.belongsToMany(models.BlogPosts, {
+      through: PostCategories,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      as: 'posts',
+    });
+  };
+
+  return PostCategories;
+};
