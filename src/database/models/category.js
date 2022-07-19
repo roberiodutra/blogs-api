98504c1854c257815e25ae13d1f14eaf6c3d23c4
roberio <roberio-almeida@hotmail.com>
2022-07-19 16:02:53 -0300

    fix: config categoryModel name

---
+++ src/database/models/category.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const categories = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  }, {
+    tableName: 'Categories',
+    timestamps: false,
+  });
+
+  return categories;
+};
