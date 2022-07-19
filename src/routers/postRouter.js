d291cb6967b12b084f4590aa7ab3c4d379c1c128
roberio <roberio-almeida@hotmail.com>
2022-07-19 20:21:49 -0300

    feat: add 'get' route for post

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -3,5 +3,6 @@ const postController = require('../controllers/postController');
 const postRouter = express.Router();
 
 postRouter.post('/', postController.add);
+postRouter.get('/', postController.getAll);
 
 module.exports = postRouter;
