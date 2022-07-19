df184c3bce426aca95ef75a1314771551c433600
roberio <roberio-almeida@hotmail.com>
2022-07-19 16:01:34 -0300

    fix: config categoryModel name

--- src/database/models/PostCategories.js
+++ src/database/models/PostCategories.js
@@ -8,14 +8,14 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   PostCategories.associate = (models) => {
-    models.BlogPosts.belongsToMany(models.Categories, {
+    models.BlogPosts.belongsToMany(models.Category, {
       through: PostCategories,
       foreignKey: 'postId',
       otherKey: 'categoryId',
       as: 'categories',
     });
 
-    models.Categories.belongsToMany(models.BlogPosts, {
+    models.Category.belongsToMany(models.BlogPosts, {
       through: PostCategories,
       foreignKey: 'categoryId',
       otherKey: 'postId',
