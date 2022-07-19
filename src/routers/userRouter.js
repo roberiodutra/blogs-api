7f31c143cb62ac8fe69720676ba998d08202bd64
roberio <roberio-almeida@hotmail.com>
2022-07-19 04:39:53 -0300

    feat: configure 'get' route for user service

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController');
 const userRouter = express.Router();
 
 userRouter.post('/', userController.create);
+userRouter.get('/', userController.getAll);
 
 module.exports = userRouter;
