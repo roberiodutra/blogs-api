76a3aefb979d6730fa462872f311a13a5c49722f
roberio <roberio-almeida@hotmail.com>
2022-07-19 22:39:52 -0300

    feat: add 'delete' route for user

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,5 +7,6 @@ const userRouter = express.Router();
 userRouter.post('/', userController.create);
 userRouter.get('/', userController.getAll);
 userRouter.get('/:id', userController.getById);
+userRouter.delete('/me', userController.remove);
 
 module.exports = userRouter;
