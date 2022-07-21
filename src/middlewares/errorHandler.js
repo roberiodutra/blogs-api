3b30d3dc224402a070b246b0a40861f49a12c52f
roberio <roberio-almeida@hotmail.com>
2022-07-21 10:41:00 -0300

    refactor: remove console.log

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,6 @@
 const { httpStatus } = require('../helpers');
 
 module.exports = (err, _req, res, _next) => {
-  console.log(err.details);
   res.status(
     err.details[0].type || httpStatus.INTERNAL_SERVER,
   )
