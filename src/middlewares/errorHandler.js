755b673cd445b664209252a4f08e8d3e45e52c46
roberio <roberio-almeida@hotmail.com>
2022-07-21 04:20:59 -0300

    refactor: change errorHandler exports mode

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,4 @@
-const errorHandler = (err, _req, res, _next) => {
+module.exports = (err, _req, res, _next) => {
   return res.status(err.message.slice(-3))
     .json({ message: err.message.slice(0, -13) });
 }
-
-module.exports = errorHandler;
