fcb3aa6e1b42305f99c2254464723f6af05802ad
roberio <roberio-almeida@hotmail.com>
2022-07-21 10:40:03 -0300

    refactor: error handlerstatus

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,9 @@
+const { httpStatus } = require('../helpers');
+
 module.exports = (err, _req, res, _next) => {
+  console.log(err.details);
   res.status(
-    err.details[0].type,
+    err.details[0].type || httpStatus.INTERNAL_SERVER,
   )
     .json({
       message: err.message,
